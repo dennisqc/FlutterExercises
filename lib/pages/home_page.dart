489c258ab4313768d076ff077285b7367c7f69a5
unknown <dennisquispe72>
2024-07-10 00:08:00 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double sliderValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            children: [
+              Slider(
+                  value: sliderValue,
+                  min: 0,
+                  max: 100,
+                  onChanged: (mandarina) {
+                    sliderValue = mandarina;
+                    print(mandarina);
+                    setState(() {});
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
